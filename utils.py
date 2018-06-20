c331f909d2fa8472cfc3ac8a82d90e35c708224d
Ed Cardinal <ed.cardinal@wdc.com>
2018-06-20 16:42:17 -0700

    Rewrote logging setup to simplify and mitigate duplicate message issues.

--- utils.py
+++ utils.py
@@ -10,23 +10,34 @@ General utilities, particularly logging
 """
 
 
-def initialize_logging(file_log_level=logging.DEBUG, console_log_level=logging.INFO,
+def initialize_logging(log_id=None, file_log_level=logging.DEBUG, console_log_level=logging.INFO,
         ci_log_name=os.environ.get('CI_LOG_NAME', 'common.log'),
         fw_root=os.environ.get('WORKSPACE', '.')):
-
+    """
+    This implementation is in-between using the basic logger and
+    a fully custom system. Since we don't have a nice module
+    structure, the __name__ trick doesn't help us and this has one
+    less layer that can get mis-configured.
+    """
+    if log_id is None:
+        # Use the root logger.
+        logger = logging.getLogger()
+    else:
+        # This is so you can give it a name (log_id) if you insist, and
+        # it will derive from the root. The log file will be named after
+        # the log_id + '.log'
+        logger = logging.getLogger(log_id)
+        ci_log_name = "{}.log".format(log_id)
     # Log name and path
     log_file = Path(fw_root) / ci_log_name
     log_file.parent.mkdir(parents=True, exist_ok=True)
-    # The root logger. Since we don't have a nice module structure, the
-    # __name__ trick doesn't help us.
-    logger = logging.getLogger()
     # Set the overall lowest level to report
     logger.setLevel(logging.DEBUG)
     # Start with no handlers - This can be the cause of duplicate messages
     logger.handlers = []
-    # File handler which logs everything
+    # File handler, logs everything
     fh = logging.handlers.RotatingFileHandler(str(log_file), mode='a',
-        maxBytes=1024 * 1024, backupCount=5)
+        maxBytes=10 * 1024 * 1024, backupCount=5)
     fh.setLevel(file_log_level)
     file_formatter = logging.Formatter(
         '%(asctime)s - %(module)s.%(funcName)s() - %(levelname)s - %(message)s')
@@ -38,5 +49,5 @@ def initialize_logging(file_log_level=logging.DEBUG, console_log_level=logging.I
     console_formatter = logging.Formatter('%(levelname)s - %(message)s')
     ch.setFormatter(console_formatter)
     logger.addHandler(ch)
-
     return logger
+
